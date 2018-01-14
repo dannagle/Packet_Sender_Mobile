73af08812f3f43a2de475b6f03c4c152f0c8faef
Dan Nagle <dan@dannagle.com>
2018-01-14 08:39:10 -0600

    fix android assembly info

--- Droid/Properties/AssemblyInfo.cs
+++ Droid/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Packet_Sender_Mobile.Droid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Packet Sender")]
+[assembly: AssemblyDescription("Network utility for sending/receiving TCP, UDP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NagleCode, LLC")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Dan Nagle")]
+[assembly: AssemblyCopyright("NagleCode, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
